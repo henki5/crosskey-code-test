fdd80f686f485330001e3e95e3d0465dd2760f9a
henki <henkelundell@gmail.com>
2024-02-20 21:48:05 +0200

    Specified Locale

--- src/main/java/MortgageApplication.java
+++ src/main/java/MortgageApplication.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Locale;
 
 public class MortgageApplication {
     public static void main(String args[]) {
@@ -24,7 +25,7 @@ public class MortgageApplication {
             MortgageData md = dataList.get(i);
             double monthlyPayment = MortgageCalculator.monthlyPayment(md);
             result += "*".repeat(100) + "\n";
-            result += String.format("Prospect %d: %s wants to borrow %f € for a period of %d years and pay %f € each month\n",
+            result += String.format(Locale.ENGLISH, "Prospect %d: %s wants to borrow %f € for a period of %d years and pay %f € each month\n",
                     i, md.getCustomerName(), md.getTotalLoan(), md.getYears(), monthlyPayment);
         }
         result += "*".repeat(100) + "\n";
--- src/test/java/MortgageParserTest.java
+++ src/test/java/MortgageParserTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MortgageParserTest {
@@ -7,7 +9,7 @@ class MortgageParserTest {
     @Test
     void getData() {
         MortgageData md1 = new MortgageData("Peter,Pan", 3500, 0.032, 2);
-        String test1 = String.format("\"%s\",%f,%f,%d",
+        String test1 = String.format(Locale.ENGLISH,"\"%s\",%f,%f,%d",
                 md1.getCustomerName(), md1.getTotalLoan(), 100*md1.getAnnualInterest(), md1.getYears());
         MortgageParser parser1 = new MortgageParser(test1);
         MortgageData md2 = parser1.getData().get(0);
